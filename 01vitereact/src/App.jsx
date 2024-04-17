b243eddce70e4c089619575a0a987f9b5bdb7b50
BhuvanMM <124192779+BhuvanMM@users.noreply.github.com>
2024-04-17 14:07:51 +0000

    basics of react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+
+  return (
+   <h1>Bhuvan is learning react using vite | BB</h1>
+       
+  )
+}
+
+export default App
---
+++ basics/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Bhuvan MM is learning react </h1>
+  );
+}
+
+export default App;
