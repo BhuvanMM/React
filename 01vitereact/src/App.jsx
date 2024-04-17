2d9d66bb18e7712a1a2b65572c364fa5f1235ea7
BhuvanMM <124192779+BhuvanMM@users.noreply.github.com>
2024-04-17 14:57:18 +0000

    few more basics

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,14 @@
+import Sample from "./sample"
+
 function App() {
 
   return (
-   <h1>Bhuvan is learning react using vite | BB</h1>
+    <>
+    <Sample/>
+    <h1> This is h1 tag</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+    </>
+   
        
   )
 }
---
+++ 01vitereact/src/sample.jsx
@@ -0,0 +1,8 @@
+function Sample()
+{
+    return(
+        <h3>Hello from sample file</h3>
+    )
+}
+
+export default Sample
