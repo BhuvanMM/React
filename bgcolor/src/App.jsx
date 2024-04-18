bc152c5be5fd9370f2c0fcf9dc47bd032e0189d2
BhuvanMM <124192779+BhuvanMM@users.noreply.github.com>
2024-04-18 06:30:09 +0000

    project-2

---
+++ bgcolor/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [color, setColor] = useState("black")
+
+  return (
+   <div className='w-full h-screen duration-200' 
+   style={{backgroundColor:color}}>
+    <div className='fixed flex flex-wrap inset-x-0 px-2 bottom-12 justify-center'>
+      <div className='flex flex-wrap px-3 py-4 space-x-3 bg-white rounded-xl'>
+        <button onClick={()=>setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+        style={{backgroundColor:"red"}}> Red</button>
+        <button onClick={()=>setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+        style={{backgroundColor:"green"}}> Green</button>
+        <button onClick={()=>setColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+        style={{backgroundColor:"blue"}}> Blue</button>
+      </div>
+    </div>
+   </div>
+  )
+}
+
+export default App
